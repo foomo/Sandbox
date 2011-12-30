35894796c5f92425c4ed12c64c36f9648571898a
Jan Halfar <jan@bestbytes.com>
2011-12-30 15:35:13 +0100

    module update

--- pullAllRecursively.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# submodule in the first level
-git submodule foreach git submodule init
-git submodule foreach git submodule update
-# submodule in the second level
-git submodule foreach git submodule foreach git submodule init
-git submodule foreach git submodule foreach git submodule update
-
